a4a60a7cd2ccfa4261c0d14921841cc87da62a0c
Vivek-k7 <vivekkashyap.kashyap124@gmail.com>
2026-03-16 22:03:17 +0530

    Add output redirection using dup2 and open

--- myshell_experimental.c
+++ myshell_experimental.c
@@ -4,36 +4,50 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
-
+#include <fcntl.h>
 int main(){
     char cmd[256];
     char *args[64];
     while(1){
-        printf("myshell> ");
-        fgets(cmd, sizeof(cmd), stdin);
+    printf("myshell> ");
+    fgets(cmd, sizeof(cmd), stdin);
+    int i = 0;
+    char* token = strtok(cmd, " \t\n");
+    while (token != NULL) {
+    args[i] = token;
+    token = strtok(NULL, " \t\n");
+    i++;
+    }
+
+    args[i] = NULL;
+    if(args[0] == NULL) continue;
+    if(strcmp(args[0], "exit") == 0) exit(0);
+    if(strcmp(args[0], "cd") == 0){
+        int ret = chdir(args[1]);
+        if (ret != 0){
+            printf("No such file or directory\n");
+        } 
+        continue;
+    }
+    pid_t  pid = fork();
+    
+    if(pid == 0){
+        int file = -1;
         int i = 0;
-        char* token = strtok(cmd, " \t\n");
-        while (token != NULL) {
-        args[i] = token;
-        token = strtok(NULL, " \t\n");
-        i++;
+        for(; args[i] != NULL; i++){
+            if(strcmp(args[i], ">") == 0){
+                file = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                break;
+            }
         }
-        args[i] = NULL;
-        if(args[0] == NULL) continue;
-        if(strcmp(args[0], "exit") == 0) exit(0);
-        if(strcmp(args[0], "cd") == 0){
-            int ret = chdir(args[1]);
-            if (ret != 0){
-                printf("No such file or directory\n");
-            } 
-            continue;
-        }
-        pid_t  pid = fork();
         
-        if(pid == 0){
-            
-            execvp(args[0], args);
-            perror("execvp");
+        if(file != -1){
+            args[i] = NULL;
+            dup2(file, 1);
+            close(file); 
+        }
+        execvp(args[0], args);
+        perror("execvp");
         }
         else{
             wait(NULL);
