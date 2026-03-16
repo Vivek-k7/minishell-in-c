c6b64c383fe0fda3364a76634e464ea9e0caf0ba
Vivek-k7 <vivekkashyap.kashyap124@gmail.com>
2026-03-16 21:57:24 +0530

    Add command parsing using strtok

--- myshell_experimental.c
+++ myshell_experimental.c
@@ -1,9 +1,35 @@
 #include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(){
     char cmd[256];
+    char *args[64];
     while(1){
         printf("myshell> ");
         fgets(cmd, sizeof(cmd), stdin);
+        int i = 0;
+        char* token = strtok(cmd, " \t\n");
+        while (token != NULL) {
+            args[i] = token;
+            token = strtok(NULL, " \t\n");
+            i++;
+        }
+
+        args[i] = NULL;
+        if(args[0] == NULL) continue;
+        
+        pid_t  pid = fork();
+        
+        if(pid == 0){
+            execvp(args[0], args);
+            perror("execvp");
+        }
+        else{
+            wait(NULL);
+        }
     }
 }
