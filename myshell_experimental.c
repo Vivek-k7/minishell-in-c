17d4ba54cb4ba87f1563a8929b60184df8629e69
Vivek-k7 <vivekkashyap.kashyap124@gmail.com>
2026-03-16 22:00:47 +0530

    Add implementation of built-in commands cd and exit

--- myshell_experimental.c
+++ myshell_experimental.c
@@ -14,17 +14,24 @@ int main(){
         int i = 0;
         char* token = strtok(cmd, " \t\n");
         while (token != NULL) {
-            args[i] = token;
-            token = strtok(NULL, " \t\n");
-            i++;
+        args[i] = token;
+        token = strtok(NULL, " \t\n");
+        i++;
         }
-
         args[i] = NULL;
         if(args[0] == NULL) continue;
-        
+        if(strcmp(args[0], "exit") == 0) exit(0);
+        if(strcmp(args[0], "cd") == 0){
+            int ret = chdir(args[1]);
+            if (ret != 0){
+                printf("No such file or directory\n");
+            } 
+            continue;
+        }
         pid_t  pid = fork();
         
         if(pid == 0){
+            
             execvp(args[0], args);
             perror("execvp");
         }
